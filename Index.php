b8a74e66d744e6afdc6e50c8380e90fef57eca02
rast20 <ramot.stephanus@gmail.com>
2022-02-11 12:36:28 +0700

    update soal

--- Index.php
+++ Index.php
@@ -1,6 +1,7 @@
 <html>
 <body>
 <p>Ganti tulisan _ganti nama anda_ di berkas php dengan nama Anda. Setelah itu edit berkas ini untuk meng-include berkas Anda di bawah. Sebagai contoh nama saya sudah masuk di nomor 1.</p>
+<h3>Semua commit harus tetap berada dalam branch main!</h3>
 
 <p>Edited by:<br/>
 	<ol><li><?php include 'Ramot.php'; ?></li>
